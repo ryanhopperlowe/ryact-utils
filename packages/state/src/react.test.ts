0f091d1741b919e3bc0cdabe31782c70b8907915
Ryan Hopper-Lowe <46546486+ryanhopperlowe@users.noreply.github.com>
2025-12-08 22:30:04 -0600

    tests for hooks

--- packages/state/src/react.test.ts
+++ packages/state/src/react.test.ts
@@ -1,7 +1,8 @@
 import { renderHook } from '@testing-library/react';
 import { test as baseTest, describe } from 'vitest';
 import { SnapshotStore } from './external-store';
-import { useShallowStore, useStore } from './react';
+import { useShallowStore, useStore, useSync } from './react';
+import { store } from './decorators';
 
 const test = baseTest.extend<{
 	store: { store: SnapshotStore<{ count: number }>; increment: () => void };
@@ -43,5 +44,33 @@ describe(SnapshotStore, () => {
 		rerender();
 
 		expect(result.current).toBe(1);
+	});
+
+	baseTest(useSync, ({ expect }) => {
+		@store
+		class MyStore {
+			count = 0;
+			increment = () => (this.count += 1);
+		}
+
+		const mystore = new MyStore();
+
+		const { result, rerender } = renderHook(() => useSync(mystore));
+
+		expect(result.current.count).toBe(0);
+		result.current.increment();
+		rerender();
+		expect(result.current.count).toBe(1);
+	});
+
+	baseTest('useSync error', ({ expect }) => {
+		class NonStoreClass {
+			count = 0;
+			increment = () => (this.count += 1);
+		}
+
+		const mystore = new NonStoreClass();
+
+		expect(() => useSync(mystore)).toThrowError();
 	});
 });
